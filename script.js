ea80f5bd463a61ef4d199a35ae30d3361ef49c26
milamim99 <camila.muniz06@escola.pr.gov.br>
2025-03-25 11:37:24 -0300

    .

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded',function () {
+    const aumentFonteBotao = document.getElementById('aumenta.fonte')
+    const diminuiFonteBotao = document.getElementById('diminui.fonte')
+
+let tamanhoaAtualFonte = 1;
+aumentaFonteBotao.addEventListener()
+   tamanhoAtualFonte += 0.1;
+   document.body.style.fontSize =  `${tamanhoaAtualFonte}rem`;
+
+});
+
+
+diminuiFonteBotao.addEventListener(´click´.function () {
+   tamanhoaAtualFonte -= 0.1;
+   document.body.style.fontSize =  ${tamanhoAtualFonte}rem´;
+});
+
+))
